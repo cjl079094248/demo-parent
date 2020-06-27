e5b1aac810d9c365c663a469cf7621b632fc6c01
caijingliang <85316320@qq.com>
2020-06-27 10:03:58 +0800

    first commit

---
+++ demo-mq-consumer/src/main/java/com/demo/component/DirectReceiver.java
@@ -0,0 +1,22 @@
+package com.demo.component;
+
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * 对于直连的交换机，多个RabbitListener（监听器）会轮询的方式对消息进行消费，而且不存在重复消费。
+ */
+@Component
+@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+public class DirectReceiver {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }
+
+}
