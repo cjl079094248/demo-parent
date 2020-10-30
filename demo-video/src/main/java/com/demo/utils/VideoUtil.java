69d5e327b1b1dfeae35c4b3b8308e52172c99b0f
caijingliang <admin@ksz>
2020-10-30 14:14:01 +0800

    视频处理

---
+++ demo-video/src/main/java/com/demo/utils/VideoUtil.java
@@ -0,0 +1,54 @@
+package com.demo.utils;
+
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+
+public class VideoUtil {
+
+    public static RenderedImage FrameToBufferedImage(Frame frame) {
+        //创建BufferedImage对象
+        Java2DFrameConverter converter = new Java2DFrameConverter();
+        BufferedImage bufferedImage = converter.getBufferedImage(frame);
+        return bufferedImage;
+    }
+
+    public static String getTempPath(String tempPath, String filePath) throws Exception {
+        File targetFile = new File(tempPath);
+        if (!targetFile.getParentFile().exists()) {
+            targetFile.getParentFile().mkdirs();
+        }
+        File file2 = new File(filePath);
+        if (file2.exists()) {
+            FFmpegFrameGrabber ff = new FFmpegFrameGrabber(file2);
+            ff.start();
+            int ftp = ff.getLengthInFrames();
+            int flag=0;
+            Frame frame = null;
+            while (flag <= ftp) {
+                //获取帧
+                frame = ff.grabImage();
+                //过滤前3帧，避免出现全黑图片
+                if ((flag>3)&&(frame != null)) {
+                    break;
+                }
+                flag++;
+            }
+            ImageIO.write(FrameToBufferedImage(frame), "jpg", targetFile);
+            ff.close();
+            ff.stop();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String tempPath = "C:/Users/sbs/Desktop/1030/1030.jpg";
+        String filePath = "C:/Users/sbs/Desktop/1030/IMG_5761.MOV";
+        getTempPath(tempPath,filePath);
+    }
+}
