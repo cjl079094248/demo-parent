3085f8daed013fbd9a03d139b5fd27ea68c4615f
caijingliang <85316320@qq.com>
2020-12-24 15:22:28 +0800

    添加sentinel的demo

--- demo-sentinel/src/main/java/com/demo/sentinel/DemoSentinelApplication.java
+++ demo-sentinel/src/main/java/com/demo/sentinel/DemoSentinelApplication.java
@@ -1,11 +1,19 @@
 package com.demo.sentinel;
 
+import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoSentinelApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoSentinelApplication.class,args);
+    }
+
+    // 注解支持的配置Bean
+    @Bean
+    public SentinelResourceAspect sentinelResourceAspect() {
+        return new SentinelResourceAspect();
     }
 }
